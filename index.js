c0866e9d3a51a0c0338b957aeda16b363a4950fc
haydanu <heh.danu@gmail.com>
2021-03-18 20:45:50 +0800

    Jawaban exercise 2 Sesi 7

--- index.js
+++ index.js
@@ -29,3 +29,30 @@ function cekNilaiYangSamaDariDuaArray(array1, array2) {
 };
 
 cekNilaiYangSamaDariDuaArray(arrayPertama, arrayKedua);
+
+/*
+Buatlah sebuah data array yang berisi nama pada tim teman-teman.
+Bagi tim menjadi 2 tim dan terdiri dari 1 orang leader
+Console.log data array tersebut dengan memisahkan setiap data menggunakan  koma (,)
+Tampilkan list data tersebut ke halaman HTML
+hidden:
+Cek built-in method Array
+Bisa pakai slice() atau splice()
+*/
+
+const namaTim = [
+  'Arsita - Leader',
+  'Fajrin - Anggota',
+  'Luthfi - Leader',
+  'Alvito - Anggota',
+];
+
+// const timPertama = namaTim.slice(0, 2);
+// const timKedua = namaTim.splice(2);
+
+// console.log(`${timPertama[0]}, ${timPertama[1]}`);
+// console.log(`${timKedua[0]}, ${timKedua[1]}`);
+
+namaTim.forEach((data) => {
+  document.write(data);
+})
